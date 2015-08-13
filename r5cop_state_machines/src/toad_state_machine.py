e31cb065efc9c1c5b32d96b6dd02449e992f8c85
ZdenekM <imaterna@fit.vutbr.cz>
2015-08-13 15:33:41 +0200

    pr2/toad simple brain

--- r5cop_state_machines/src/toad_state_machine.py
+++ r5cop_state_machines/src/toad_state_machine.py
@@ -6,30 +6,23 @@ from zmq_object_exchanger import zmqObjectExchanger
 import time
 import tf
 import actionlib
-import frontier_exploration.msg
 from robot_position import robotPosition
     
-class PR2RobotBrain():
+class ToadRobotBrain():
 
     def __init__(self, boundary, center):
     
         self.tfl = tf.TransformListener()
         
-        self.comm = zmqObjectExchanger("PR2", "pr2", 1234)
-        self.comm.zmq.add_remote("Toad", "toad", 1234)
-        
-        self.boundary = boundary
-        self.center = center
-        self.action_client = actionlib.SimpleActionClient('explore_server', frontier_exploration.msg.ExploreTaskAction)
+        self.comm = zmqObjectExchanger("Toad", "toad", 1234)
+        self.comm.zmq.add_remote("PR2", "pr2", 1234)
         
         rospy.Timer(rospy.Duration(0.25), outDataTimer)
         rospy.Timer(rospy.Duration(0.25), inDataTimer)
         
-        self.stop_exploring = False
-        
     def getReady(self):
     
-        self.action_client.wait_for_server()
+        pass
     
     def outDataTimer(self, evt):
     
@@ -56,31 +49,11 @@ class PR2RobotBrain():
                 # TODO store it somehow
                 pass            
     
-    def explorationFeedbackCallback(self, fb):
-  
-        pass
-        
-    def explore(self):
-    
-        while not self.stop_exploring:
-    
-            goal = frontier_exploration.msg.ExploreTaskGoal()
-            goal.explore_boundary = self.boundary
-            goal.explore_center = self.center
-            
-            self.action_client.send_goal(goal, explorationFeedbackCallback)
-            
-            while not (self.stop_exploring or self.client.wait_for_result(time.Duration(0.25))):
-            
-                if self.stop_exploring:
-                    self.action_client.cancel_goal()
-                    
-        self.stop_exploring = False
     
 def main():
   
-    rospy.init_node('pr2_state_machine')
-    brain = PR2RobotBrain()
+    rospy.init_node('toad_state_machine')
+    brain = ToadRobotBrain()
     brain.getReady()
     
     rospy.spin()
