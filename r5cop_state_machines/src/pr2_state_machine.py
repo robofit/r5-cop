de1166a3603c88aa94b475b1055f88a6bec5bf96
Materna Zdenek <imaterna@fit.vutbr.cz>
2015-08-17 11:30:17 +0200

    working exploration + basic information sharing

--- r5cop_state_machines/src/pr2_state_machine.py
+++ r5cop_state_machines/src/pr2_state_machine.py
@@ -2,12 +2,13 @@
 
 import roslib; roslib.load_manifest('r5cop_state_machines')
 import rospy
-from zmq_object_exchanger import zmqObjectExchanger
+from zmq_object_exchanger.zmq_object_exchanger import zmqObjectExchanger
 import time
 import tf
 import actionlib
 import frontier_exploration.msg
 from robot_position import robotPosition
+from geometry_msgs.msg import PolygonStamped, PointStamped, Polygon, Point32
     
 class PR2RobotBrain():
 
@@ -15,15 +16,15 @@ class PR2RobotBrain():
     
         self.tfl = tf.TransformListener()
         
-        self.comm = zmqObjectExchanger("PR2", "pr2", 1234)
-        self.comm.zmq.add_remote("Toad", "toad", 1234)
+        self.comm = zmqObjectExchanger("PR2", "127.0.0.1", 1234)
+        self.comm.add_remote("Toad", "127.0.0.1", 4567)
         
         self.boundary = boundary
         self.center = center
         self.action_client = actionlib.SimpleActionClient('explore_server', frontier_exploration.msg.ExploreTaskAction)
         
-        rospy.Timer(rospy.Duration(0.25), outDataTimer)
-        rospy.Timer(rospy.Duration(0.25), inDataTimer)
+        rospy.Timer(rospy.Duration(1.0), self.outDataTimer)
+        rospy.Timer(rospy.Duration(0.25), self.inDataTimer)
         
         self.stop_exploring = False
         
@@ -33,7 +34,7 @@ class PR2RobotBrain():
     
     def outDataTimer(self, evt):
     
-        now = rospy.time.now()
+        now = rospy.Time.now()
         ps = robotPosition(self.tfl).get(now)
         
         msg = {}
@@ -53,6 +54,7 @@ class PR2RobotBrain():
         
             if msg["topic"] == "state_info":
             
+                print(msg["data"])
                 # TODO store it somehow
                 pass            
     
@@ -60,30 +62,66 @@ class PR2RobotBrain():
   
         pass
         
+    def loop(self):
+    
+        self.getReady()
+    
+        while not rospy.is_shutdown():
+        
+            # Explore until object is found
+            self.explore()
+            
+            # Object found - start manipulation / call Toad
+            # TBD
+        
+        
     def explore(self):
     
-        while not self.stop_exploring:
+        while not (self.stop_exploring or rospy.is_shutdown()):
     
             goal = frontier_exploration.msg.ExploreTaskGoal()
             goal.explore_boundary = self.boundary
             goal.explore_center = self.center
             
-            self.action_client.send_goal(goal, explorationFeedbackCallback)
+            self.action_client.send_goal(goal, feedback_cb=self.explorationFeedbackCallback)
+            rospy.loginfo("Exploration started.") 
             
-            while not (self.stop_exploring or self.client.wait_for_result(time.Duration(0.25))):
-            
-                if self.stop_exploring:
+            while not self.action_client.wait_for_result(rospy.Duration(0.25)):
+                        
+                if rospy.is_shutdown() or self.stop_exploring:
+                
                     self.action_client.cancel_goal()
-                    
+                    break
+            
+            rospy.loginfo("Exploration finished.") 
+        
         self.stop_exploring = False
     
 def main():
   
     rospy.init_node('pr2_state_machine')
-    brain = PR2RobotBrain()
-    brain.getReady()
     
-    rospy.spin()
+    area_to_explore = PolygonStamped()
+    center_point = PointStamped()
+    
+    now = rospy.Time.now()
+    
+    area_to_explore.header.stamp = now
+    area_to_explore.header.frame_id = "map"
+    points = [Point32(-1.65, -1.56, 0.0),
+              Point32(5.41, -1.7, 0.0),
+              Point32(5.57, 4.44, 0.0),
+              Point32(-1.75, 4.37, 0.0)]
+              
+    area_to_explore.polygon = Polygon(points)
+    
+    center_point.header = area_to_explore.header
+    center_point.point.x = 1.83
+    center_point.point.y = 1.57
+    center_point.point.z = 0.0
+    
+    brain = PR2RobotBrain(area_to_explore, center_point)
+    brain.loop()
     
 if __name__ == '__main__':
     main()
